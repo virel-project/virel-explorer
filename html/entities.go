5d881f30e7cd5684d047cf258a112b13f26a91d8
josetornante <209896672+josetornante@users.noreply.github.com>
2025-08-22 10:30:22 +0200

    add safetrade entity

--- html/entities.go
+++ html/entities.go
@@ -5,4 +5,5 @@ var Entities = map[string]string{
 	"v9mjh9ggoy0acio6y2ce43g0qt5s6x5dtmbv3k":  "ExPool.net",
 	"vfkpe1x0megmsaek3mhmjqfpn6buyjoa5c5hrt":  "Rplant.xyz",
 	"v139diixrpv0ftmip4mgpuy92u51iq4pnmgjsfn": "Development fund",
+	"v1csprnolatlj3t4dlgwzpgzzjlqmperl1tmfrs": "SafeTrade",
 }
