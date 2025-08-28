9e9a47408411f62654d9ae23d9284e52836b63ec
josetornante <209896672+josetornante@users.noreply.github.com>
2025-08-28 20:37:10 +0200

    add luckypool entity

--- html/entities.go
+++ html/entities.go
@@ -6,4 +6,5 @@ var Entities = map[string]string{
 	"vfkpe1x0megmsaek3mhmjqfpn6buyjoa5c5hrt":  "Rplant.xyz",
 	"v139diixrpv0ftmip4mgpuy92u51iq4pnmgjsfn": "Development fund",
 	"v1csprnolatlj3t4dlgwzpgzzjlqmperl1tmfrs": "SafeTrade.com",
+	"vjbyt6ia7gg1udmqnr3h6su4gayzxpfdjghp8v":  "LuckyPool.io",
 }
